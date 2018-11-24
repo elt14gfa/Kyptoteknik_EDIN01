7ef9e55c417669768fad1e967fd05f1ea85c7a90
joelbill <joel.bill94@gmail.com>
2018-11-24 22:51:00 +0100

    Prime factors now found, only missing solving the matrix equation

--- Project 1/test.py
+++ Project 1/test.py
@@ -1,21 +1,13 @@
 import numpy as np
 from operator import add
+from operator import mul
+import functools
 
 
-def getRowIndex(matrix, findRow):
-    counter = 0
-    for row in matrix:
-        if np.array_equal(row, findRow):
-            break
-        else:
-            counter += 1
+list = [2, 2, 2, 2, 1, 1, 2, 10]
+prod = functools.reduce(mul, list, 1)
+print(prod)
 
-    return counter
-
-
-arr = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
-index = getRowIndex(arr, [1,2,3,5])
-print('The row is on index: ', index)
 
 
 
